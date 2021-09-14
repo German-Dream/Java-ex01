223c1c4a9d88f4a9c3c3f5a483bc2946abbca8e3
German-Dream <90539687+German-Dream@users.noreply.github.com>
2021-09-14 21:55:24 +0900

    Java-ex02
    두번째 날 코드

---
+++ Ex03_Overloading/src/Calculation.java
@@ -0,0 +1,30 @@
+
+// 클래스에 같은 이름의 메서드가 여러 개 존재
+class Calc{
+	int add(int a, int b) {
+		return a + b;
+	}
+	
+	int add(int a) {
+		return a + 1;
+	}
+	
+	double add(double a, double b) {
+		return a + b;
+	}
+}
+public class Calculation {
+
+	public static void main(String[] args) {
+		Calc calc = new Calc();
+		// 메서드 호출
+		int nReturn1 = calc.add(3, 9);
+		int nReturn2 = calc.add(4);
+		double nReturn3 = calc.add(3.0, 9.0);
+		
+		System.out.println("r1 =" + nReturn1);
+		System.out.println("r2 =" + nReturn2);
+		System.out.println("r3 =" + nReturn3);
+	}
+
+}
